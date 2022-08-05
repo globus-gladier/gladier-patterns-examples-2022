1ae93210e6ea4799619639b0a5e6969062f9e6f3
Ian Foster <foster@anl.gov>
2022-08-05 15:51:19 -0500

    Update and rename ptychodus_client.py to simple_ptycho_client.py

--- simple_ptycho_client.py
+++ simple_ptycho_client.py
@@ -36,21 +36,18 @@ if __name__ == "__main__":
     sample_name = args.samplename
     data_dir = os.path.join(args.datadir, sample_name)
     run_label = "DEMO Ptycho: " + sample_name
+    
+    instrument_computer_collection_id = "a17d7fac-ce06-4ede-8318-ad8dc98edd69"
 
     # TODO: Set the following values for your environment
 
     # Your Globus Transfer endpoint UUID where data processing will be done
-    # destination_endpoint_id = "<UUID Value>"
+    analysis_computer_collection_id = "<UUID Value>"
 
     # Your FuncX endpoint UUID where the processing function can run. It must be
     # able to access the data as stored on the Globus Transfer endpoint set by
     # destination_endpoint_id
-    # funcx_endpoint_compute = "<UUID Value>"
-
-    # Your FuncX endpoint UUID where the non-compute intensive processing will
-    # run. It also must be able to access the data as stored on the Globus
-    # Transfer endpoint set by destination_endpoint_id
-    # funcx_endpoint_non_compute = "<UUID Value>"
+    analysis_computer_funcx_id = "<UUID Value>"
 
     # Base input for the flow
     flow_input = {
@@ -60,17 +57,16 @@ if __name__ == "__main__":
             "data_dir": data_dir,  # relative to endpoint
             "proc_dir": data_dir,  # relative to funcx
             # REMOTE DEMO ENDPOINT FOR PTYCHO DATA
-            "from_storage_transfer_source_endpoint_id": "a17d7fac-ce06-4ede-8318-ad8dc98edd69",
+            "from_storage_transfer_source_endpoint_id": instrument_computer_collection_id,
             "from_storage_transfer_source_path": "/PTYCHO/fly001",
-            "from_storage_transfer_destination_endpoint_id": destination_endpoint_id,
+            "from_storage_transfer_destination_endpoint_id": analysis_computer_collection_id,
             "from_storage_transfer_destination_path": str(data_dir),
             "from_storage_transfer_recursive": True,
             # shell cmd inputs
             "args": f"ptychodus -f {data_dir} -b -s ptychodus.ini > ptychodus.log",
             "cwd": f"{data_dir}",
             "timeout": 180,
-            "funcx_endpoint_non_compute": funcx_endpoint_non_compute,
-            "funcx_endpoint_compute": funcx_endpoint_compute,
+            "funcx_endpoint_compute": analysis_computer_funcx_id,
         }
     }
 
